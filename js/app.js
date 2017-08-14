3d4e4ea526e63826baca466eba010097e925ca3e
Bianjie <bianjieandara@gmail.com>
2017-08-13 20:47:43 -0400

    4

--- js/app.js
+++ js/app.js
@@ -5,9 +5,24 @@ angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController ($scope) {
-$scope.dishes= "Hola";
+$scope.dishes= "";
+$scope.message= "";
+$scope.check= function (event) {
+var array=$scope.dishes.split(',');
+var i = 0;
+array.forEach( function(valor, indice) { 
+    if(valor!=""&&valor!=" "){
+    i++;	
+    }
+});
+if(i<4){
+$scope.message= "Enjoy!";
 }
-
+else{
+$scope.message= "Too much!";	
+}
+}
+}
 
 
 
